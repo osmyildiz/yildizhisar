2a1efacd5af0c820e7ba9ec258a16159d02e95f7
focusedutr <focusedutr@gmail.com>
2021-10-25 19:26:50 +0300

    events v1

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -17,7 +17,7 @@ class EventSeeder extends Seeder
     public function run()
     {
         DB::table('events')->insert([
-            'main_text_tr'=>"Sevdikleriniz ile beraber geçireceğiniz unutulmaz anıların birikeceği hem gösterişli hem de şık görünümüyle istediğiniz kombin ile uyumlu hale getirebileceğiniz mekan alternatifi Yıldız Hisar’da! Tüm ayrıntıları sizin için düşünülmüş Boğaz manzarasında doğum günü organizasyonu için bize ulaşın. Özel günlerinizi birlikte dizayn edelim!",
+            'main_text_tr'=>"Doğum günlerinizi size özel kılacak alternatif çeşitliliği, gösterişli ve şık görünüme sahip atmosferi, her detayı sizin için düşünülmüş hizmet kalitesi ile Boğaz manzaralı organizasyonlarınız için bize ulaşabilirsiniz.",
             'main_text_en'=>"Yildiz Hisar is an alternative to the place where you can make unforgettable memories with your loved ones, which you can harmonize with the combination you want with its flashy and stylish appearance! Contact us for a birthday organization with a view of the Bosphorus, all details of which have been thought for you. Let's design your special days together!",
             'name_tr'=>"Doğum Günü",
             'name_en'=>"BIRTHDAY PARTY",
@@ -28,7 +28,7 @@ class EventSeeder extends Seeder
             "updated_at"=>date("Y-m-d"),
         ]);
         DB::table('events')->insert([
-            'main_text_tr'=>"Toplantı, seminer, lansman ve açılış organizasyonlarınızda, başarı ile tamamlanmış etkinliklere imza atabilmeniz için, ihtiyacınız olan boğaz manzaralı mekanda hijyen ve kalite standartlarına bağlı kalarak, kurumunuza yakışır şekilde sizler için hazırlanan menülerle keyifli vakit geçirmenizi sağlıyoruz.",
+            'main_text_tr'=>"Bayi toplantısı, lansman, eğitim ve dönem sonu toplantıları, seminer, açılış gibi organizasyonlarınızda katılımcı profilinize uygun hizmet kalitesi ile kurumunuzu temsil etmek için hazırız. ",
             'main_text_en'=>"In your meetings, seminars, launches and opening organizations, we ensure that you have a pleasant time with the menus prepared for you, adhering to the hygiene and quality standards, in a place with a Bosphorus view you need, in order for you to be able to sign successfully completed events.",
             'name_tr'=>"Toplantı & Lansman",
             'name_en'=>"MEETING & LAUNCHING",
@@ -39,7 +39,8 @@ class EventSeeder extends Seeder
             "updated_at"=>date("Y-m-d"),
         ]);
         DB::table('events')->insert([
-            'main_text_tr'=>"Partilerin en tatlısı; Baby Shower.. Siz anne ve babaların kucağına alacağınız bebek sevincini eşsiz boğaz manzaramızda tüm sevdiklerinizle kutlayabilmeniz için şıklık ve eğlenceyi bir arada sunarak en özel anınıza ev sahipliği yapıyoruz.",
+            'main_text_tr'=>"Partilerin en tatlısı, Baby Shower…
+Kucağınıza alacağınız bebeğinizin sevincini ve heyecanını, eşsiz Boğaz manzaramız eşliğinde tüm sevdiklerinizle birlikte doyasıya yaşamanız için hayalini kurduğunuz partiyi gerçekleştirmenizi sağlıyoruz.",
             'main_text_en'=>"The sweetest of parties; Baby Shower.. We are hosting your most special moment by offering elegance and fun together so that you can celebrate the joy of your baby in your arms with all your loved ones in our unique Bosphorus view.",
             'name_tr'=>"Baby Shower",
             'name_en'=>"BABY SHOWER",
@@ -51,7 +52,9 @@ class EventSeeder extends Seeder
             "updated_at"=>date("Y-m-d"),
         ]);
         DB::table('events')->insert([
-            'main_text_tr'=>"Kurumsal İş Yemeklerini Yıldız Hisar’da Yapın! Şirket yemeklerinin bulunduğumuz işletme ve kariyerimiz için oldukça önemli olduğunun bilincinde bir kurum olarak etkinliklerinizi unutulmayacak lezzette şirketinize uygun, kurumsal yapınıza yakışan bir organizasyonu hayata geçiriyoruz.",
+            'main_text_tr'=>"Kurumunuz için önemli etkinliklerden biri olan şirket yemeklerinize Rumeli Hisarı’nın köklü geçmişi, Boğaz’ın engin manzarası yakışır…
+
+Lezzette, hizmette ve bulunduğu konumda iddialı olan işletmemiz, kurum içi motivasyonunuzu yükseltmek için doğru bir adres!",
             'main_text_en'=>"Have Corporate Business Meals at Yıldız Hisar! Being aware of the fact that company meals are very important for our business and our career, we realize your events with an unforgettable taste, suitable for your company and befitting your corporate structure.",
             'name_tr'=>"Şirket Akşam Yemeği",
             'name_en'=>"COMPANY DINNER",
@@ -63,7 +66,7 @@ class EventSeeder extends Seeder
             "updated_at"=>date("Y-m-d"),
         ]);
         DB::table('events')->insert([
-            'main_text_tr'=>"Bekârlığa veda partini farklı kılacak bir mekan mı arıyorsun? Son yılların yükselen trendi bekarlığa veda partilerinde çok sevdiğiniz arkadaşlarınızla birlikte bekarlığın o görkemli büyüsüne yakışan bir vedayı Yıldız Hisar’da organize ediyoruz. Yıllarca unutamayacağınız ve hep birlikte yaşayacağınız keyif dolu anları hayata geçiriyoruz.",
+            'main_text_tr'=>"Bekarlığa veda partileri, yıllar boyu arkadaşlarınızla birlikte keyifle hatırlayacağınız en önemli anılardan biridir. Bu anınızı, Boğaz manzarası ile mükemmelleştirmek, eğlencenize kaliteli hizmeti de dahil etmek için ekibimiz sizleri bekliyor… ",
             'main_text_en'=>"Are you looking for a venue to make your bachelorette party different? In the rising trend of recent years, we organize a farewell in Yıldız Hisar with your beloved friends, which suits the magnificent magic of being single. We bring you joyful moments that you will not forget for years and that you will live together.",
             'name_tr'=>"Bekarlığa Veda",
             'name_en'=>"STAG NIGHT",
