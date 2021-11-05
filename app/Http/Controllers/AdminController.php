62c2baedd8b393ce95c58b19d0647100955d24fe
focusedutr <focusedutr@gmail.com>
2021-11-05 15:31:42 +0300

    resim ekleme düzeltildi

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -740,6 +740,13 @@ class AdminController extends Controller
         $wedding->name_en = $request->name_en;
         $wedding->main_text_tr = $request->main_text_tr;
         $wedding->main_text_en = $request->main_text_en;
+        if($request->hasFile('img1')){
+        $id = mt_rand(1000, 9999);
+        $imageName = $id."_".time().'.'.$request->img1->extension();
+
+        $request->img1->move(public_path('img'), $imageName);
+        $wedding->url = "/img/".$imageName;
+    }
 
         $wedding->is_active = $is_active;
 
